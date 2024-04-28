249ac1adcad8bbbede71d4817f3a5917d5a79137
levi-tek <u777886438@byui.edu>
2024-04-28 08:05:22 +0100

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string grades = Console.ReadLine();
+        int grade = int.Parse(grades);
+        string letter;
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine(letter);
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("So sorry! You failed the course, but dont worry you can still do beter next time");
+        }
     }
 }
