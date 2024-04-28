5c6c6961ff1df16ac4145506a157b86f460a72f0
levi-tek <u777886438@byui.edu>
2024-04-28 12:58:40 +0100

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when you are done.");
+        List<int> number = new List<int>();
+        Console.Write("Enter number: ");
+        string num = Console.ReadLine();
+        int numbers = int.Parse(num);
+        number.Add(numbers);
+        while (num != "0")
+        {
+            Console.Write("Enter number: ");
+            num = Console.ReadLine();
+            numbers = int.Parse(num);
+            number.Add(numbers);
+        }
+        int sum = number.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        double average = number.Average();
+        Console.WriteLine($"The average is: {average}");
+        int max = number.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+
+        
+        
     }
 }
