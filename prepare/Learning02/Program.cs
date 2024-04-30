be676e446dcb7561868e9f81578e433b56604988
levi-tek <u777886438@byui.edu>
2024-04-30 12:57:40 +0100

    week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        job job1 = new job();
+        job1._jobTitle = "Full Time Missionary";
+        job1._company = "The Church of Jesus Christ of Latter Day-Saint";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+        // job1.DisplayDetails();
+
+
+        job job2 = new job();
+        job2._jobTitle = "Field Agent ";
+        job2._company = "Family Search";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+        // job2.DisplayDetails();
+
+        resume resume = new resume();
+        resume._name = "Levi Oguike";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+        
+
+
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class resume 
+{
+    public string _name;
+    public List<job> _jobs = new List<job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (job job in _jobs)
+        {
+           job.DisplayDetails();
+        }
+    }
+}
